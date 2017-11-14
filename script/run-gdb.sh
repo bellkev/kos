53423a106b4d27a4c4d8a3dc8a7882855a1bfbbe
Kevin Bell <kevin.a.bell@gmail.com>
2017-11-13 20:41:30 -0800

    Simplify gdb docker command

--- script/run-gdb.sh
+++ script/run-gdb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 while ! nc -z localhost 1234; do echo "Waiting for gdb server"; sleep 1; done
-docker run -v $PWD:/root -it --detach-keys="ctrl-e,e" bellkev/kos bash -c 'gdb -x script/gdb-init'
+docker run -v $PWD:/root -it --detach-keys="ctrl-e,e" bellkev/kos gdb -x script/gdb-init
